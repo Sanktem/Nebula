e0aa5f6febce3bc92a2eaa9f21d4066cc8266c1e
Dale Scott <55069993+Sanktem@users.noreply.github.com>
2020-10-20 19:43:08 -0500

    updates

--- lib/cogs/Not_In_Vid.py
+++ lib/cogs/Not_In_Vid.py
@@ -1,8 +1,9 @@
 from typing import Optional
+from random import choice
 
 from aiohttp import request
 from discord import Member, Embed
-from discord.ext.commands import command, Cog, BadArgument
+from discord.ext.commands import command, Cog, BadArgument, cooldown, BucketType
 
 class Not_In_Vid(Cog):
     def __init__(self, bot):
@@ -24,7 +25,14 @@ class Not_In_Vid(Cog):
             
             else:
                 await ctx.send("No meme found")
-    
+
+    @command(name="fuck", aliases=["Fuck"])
+    @cooldown(1, 15, BucketType.guild)
+    async def fuck(self, ctx):
+        """Tells someone to fuck off"""
+        the_fucked = choice(ctx.channel.guild.members)
+        await ctx.send(f"{ctx.author.display_name} says 'Fuck you' to {the_fucked.mention}")
+
     @Cog.listener()
     async def on_ready(self):
         if not self.bot.ready:
